275b18d550ebc1b7269ea69ca042960b5e2c47bd
AneVisser <Ane@AgriVisser.com>
2026-03-13 14:29:48 +1300

    restore to original. Somehow, dairymax files got in here

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "com.qed"
--- src/main/kotlin/sut/dairymax_REST/ktor_responses/ICalendarMonth.kt
+++
@@ -1,6 +0,0 @@
-package qed.testbaseclass.sut.dairymax_REST.ktor_responses
-
-interface ICalendarMonth {
-    val monthNumber: Int
-    val shortName: String
-}
