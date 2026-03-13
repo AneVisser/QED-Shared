2d7649b338b8fa982c3d9c3d4c6a7962f95fdff7
Ane Visser <Ane@AgriVisser.com>
2026-03-13 14:10:20 +1300

    build.gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     kotlin("plugin.serialization") version "2.0.20"
 }
 
+group = "com.qed"
+version = "1.0.0"
+
 repositories {
     mavenCentral()
 }
@@ -13,12 +16,6 @@ kotlin {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-    implementation("com.squareup.moshi:moshi:1.15.2")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
-    implementation("com.squareup.moshi:moshi-adapters:1.15.2")
 }
 
-// make it publishable to the composite build
-group = "com.qed"
-version = "1.0.0"
+
