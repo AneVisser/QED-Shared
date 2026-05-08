571745d8ff046c6369f2b71881d9b25b7ddfff2b
AneVisser <Ane@AgriVisser.com>
2026-05-09 07:36:59 +1200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "com.qed"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+
